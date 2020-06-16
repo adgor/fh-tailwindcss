71938a762886c3fb2829e88af33a500c7ecb6305
adgor <adgor000@gmail.com>
2020-06-16 02:22:44 +0200

    initial commit

---
+++ public/dist/js/main.js
@@ -0,0 +1,2 @@
+const menu = document.getElementById('menu');
+const toggle = () => menu.classList.toggle("hidden");
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  theme: {
+    container: {
+      center: true,
+      padding: '2rem'
+    },
+    extend: {},
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active']
+    
+  },
+  plugins: [],
+}
